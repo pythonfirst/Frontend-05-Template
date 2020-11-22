90c4ff7d5a9a5a9fbf9c41516057d0da36f456de
canxiang.zhao <canxiang.zhao@seek4paws.com>
2020-11-22 11:53:52 +0800

    finish realms

---
+++ Week 07/typeConvert.js
@@ -0,0 +1,10 @@
+function StringToNumber(str) {
+  return parseInt(str)
+}
+
+function NumberToString(num, radix) {
+  if (radix >= 2 && radix <= 36) {
+    let number = new Number(num)
+    return number.toString(radix)
+  }
+}
